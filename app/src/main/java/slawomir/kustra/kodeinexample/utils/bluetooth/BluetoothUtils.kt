02d73c503bb46f234fc73223c6474da3970532b8
skustra <kustraslawomir@gmail.com>
2018-11-18 20:56:19 +0100

    bluetooth permissions utils

---
+++ app/src/main/java/slawomir/kustra/kodeinexample/utils/bluetooth/BluetoothUtils.kt
@@ -0,0 +1,21 @@
+package slawomir.kustra.kodeinexample.utils.bluetooth
+
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import slawomir.kustra.kodeinexample.utils.Constants
+
+class BluetoothUtils {
+
+    companion object {
+
+        fun checkBluetooth(bluetoothAdapter: BluetoothAdapter?): Boolean {
+            return !(bluetoothAdapter == null || !bluetoothAdapter.isEnabled)
+        }
+
+        fun requestBluetooth(activity: AppCompatActivity) {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            activity.startActivityForResult(enableIntent, Constants.REQUEST_ENABLE_BT)
+        }
+    }
+}
